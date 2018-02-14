a69d8ff8642694e693444783799d9ed4f00c9900
Unknown <mswenson@andrew.cmu.edu>
2018-02-13 20:13:04 -0500

    linted files because it was bothering me

--- hw1/code/scripts/MotionModel.py
+++ hw1/code/scripts/MotionModel.py
@@ -20,10 +20,10 @@ class MotionModel:
         self.alpha_3 = 0.5  # linear
         self.alpha_4 = 0.5  # linear
 
-        #self.alpha_1 = 0.0  # rotation
-        #self.alpha_2 = 0.0  # rotation
-        #self.alpha_3 = 0.0  # linear
-        #self.alpha_4 = 0.0  # linear
+        # self.alpha_1 = 0.0  # rotation
+        # self.alpha_2 = 0.0  # rotation
+        # self.alpha_3 = 0.0  # linear
+        # self.alpha_4 = 0.0  # linear
 
         self.mu = 0.0  # zero mean noise for sampling
 
--- hw1/code/scripts/SensorModel.py
+++ hw1/code/scripts/SensorModel.py
@@ -20,18 +20,18 @@ class SensorModel:
         TODO : Initialize Sensor Model parameters here
         """
         self.norm_std = 300.
-        self.max_range = 8183. # Zmax = 8333
-        self.lambdaH=0.001
-        
-        self.sqrt2=math.sqrt(2.)
-        self.div1=self.norm_std*self.sqrt2
+        self.max_range = 8183.  # Zmax = 8333
+        self.lambdaH = 0.001
+
+        self.sqrt2 = math.sqrt(2.)
+        self.div1 = self.norm_std * self.sqrt2
         self.map = map_reader
 
-        probShort = 0.00  
+        probShort = 0.00
         probMax = 0.11
         probRand = 0.11
         probHit = 1. - (probShort + probMax + probRand)
-                
+
         # relative weights of each distribution in final pseudodistribution
         self.scale = (probHit, probShort, probMax, probRand)
 
@@ -56,16 +56,18 @@ class SensorModel:
             prob = list()
 
             # Auxiliar local variables
-            self.x1=-z_r+self.max_range
-            self.x2=(z_t-z_r)/self.norm_std
+            self.x1 = -z_r + self.max_range
+            self.x2 = (z_t - z_r) / self.norm_std
 
-            up=math.sqrt(2./math.pi)*math.exp(-1./2.*self.x2*self.x2)
-            down=self.norm_std*(math.erf(z_r/self.div1)+math.erf(self.x1/self.div1))
+            up = math.sqrt(2. / math.pi) * math.exp(-1. / 2. * self.x2 * self.x2)
+            down = self.norm_std * (math.erf(z_r / self.div1) + math.erf(self.x1 / self.div1))
 
-            unexpected=math.exp(-self.lambdaH*min(z_t,z_r))/(1.-math.exp(-z_r*self.lambdaH))           
-            
-            if z_r>=self.max_range: self.flag=1.
-            else: self.flag=0.   
+            unexpected = math.exp(-self.lambdaH * min(z_t, z_r)) / (1. - math.exp(-z_r * self.lambdaH))
+
+            if z_r >= self.max_range:
+                self.flag = 1.
+            else:
+                self.flag = 0.
 
             if(z_r >= self.max_range - self.deltaRes / 2) and (z_r <= self.max_range + self.deltaRes / 2):
                 self.flag = 1.
@@ -73,11 +75,11 @@ class SensorModel:
                 self.flag = 0.
 
             # Probabilities
-            prob.append(up/down)
+            prob.append(up / down)
             prob.append(unexpected)
             prob.append(self.flag)
-            prob.append(1./self.max_range)
-        
+            prob.append(1. / self.max_range)
+
             for p, s in zip(prob, self.scale):
                 q += p * s  # might need to use log sum here...
             # print(down)
